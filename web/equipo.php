8c5427a5f6cbac9e49f3b35c8a45e8126b715911
Edison <santy_g33@hotmail.com>
2018-05-14 16:38:32 +0200

    subiendo la web

---
+++ web/equipo.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">	
+		<title>Inicio - Sucrecoin</title>	
+		<meta name="keywords" content="HTML5 Template" />
+		<meta name="description" content="Sucrecoin Primera Cryptomoneda Latinoamericana">
+		<meta name="author" content="vps593.com">
+		<?php include("includes/head.php"); ?>
+	</head>
+	<body>
+	<div class="body">
+	<header id="header" data-plugin-options='{"stickyEnabled": true, "stickyEnableOnBoxed": true, "stickyEnableOnMobile": true, "stickyStartAt": 57, "stickySetTop": "-57px", "stickyChangeLogo": true}'>
+		<?php include("includes/menu.php"); ?>
+	</header>
+
+			<div role="main" class="main">
+
+				<section class="page-header">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-12">
+								<ul class="breadcrumb">
+									<li><a href="#">Inicio</a></li>
+									<li class="active">Equipo</li>
+								</ul>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h1>Nuestro equipo</h1>
+							</div>
+						</div>
+					</div>
+				</section>
+
+				<div class="container">
+
+									<ul class="nav nav-pills sort-source" data-sort-id="team" data-option-key="filter">
+						<li data-option-value="*" class="active"><a href="#">Mostrar todos</a></li>
+						<li data-option-value=".ceo"><a href="#">CEO</a></li>
+						<li data-option-value=".programador"><a href="#">Programadores</a></li>
+						<li data-option-value=".consultor"><a href="#">Consultores</a></li>
+						<li data-option-value=".diseno"><a href="#">Diseñadores</a></li>
+					</ul>
+
+					<hr>
+
+					<div class="row">
+
+						<div class="sort-destination-loader sort-destination-loader-showing">
+							<ul class="team-list sort-destination" data-sort-id="team">
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item ceo">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="#">
+												<img src="img/team/team-1.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">Carlos Gamarra</span>
+													<span class="thumb-info-type">CEO | Fundador</span>
+													<span class="thumb-info-type">Guayaquil - Ecuador</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Chief of Company Telecomunication and Security.</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item programador">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="#">
+												<img src="team/marco.png" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">Marco Triviño</span>
+													<span class="thumb-info-type">Programador Sénior</span><span class="thumb-info-type">Guayaquil - Ecuador</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Programador Analista de Sistema con 20 años de experiencia</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item programador">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="#">
+												<img src="team/gio.png" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner"><small>Tec.</small> Giovanni Orellana</span>
+													<span class="thumb-info-type">Programador Júnior</span><span class="thumb-info-type">El Salvador - San Salvador </span>
+												</span>
+											</a>
+										</span><br>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Programador Junnior con 2 años de experiencia</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="https://www.facebook.com/ifgiovanni/"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com/ifgiovanni"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item consultor">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="#">
+												<img src="img/team/team-4.jpg" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner">MSC. Christian Rosado</span>
+													<span class="thumb-info-type">Consultor Informático</span>
+												<span class="thumb-info-type">	Otavalo - Ecuador
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">MSC, con experiencia en Analisis de Procesos. </span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								<li class="col-md-3 col-sm-6 col-xs-12 isotope-item diseno">
+									<span class="thumb-info thumb-info-hide-wrapper-bg mb-xlg">
+										<span class="thumb-info-wrapper">
+											<a href="#">
+												<img src="team/luis.png" class="img-responsive" alt="">
+												<span class="thumb-info-title">
+													<span class="thumb-info-inner"> <small>ING</small> Luis Emilio Figueroa</span>
+													<span class="thumb-info-type">Diseñador</span>
+													<span class="thumb-info-type">Pedro Carbo - Ecuador</span>
+												</span>
+											</a>
+										</span>
+										<span class="thumb-info-caption">
+											<span class="thumb-info-caption-text">Consultor Marketing Digital & Diseño Grafico</span>
+											<span class="thumb-info-social-icons">
+												<a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i><span>Facebook</span></a>
+												<a href="http://www.twitter.com"><i class="fa fa-twitter"></i><span>Twitter</span></a>
+												<a href="http://www.linkedin.com"><i class="fa fa-linkedin"></i><span>Linkedin</span></a>
+											</span>
+										</span>
+									</span>
+								</li>
+								
+							</ul>
+						</div>
+
+					</div>
+
+				</div>
+
+			</div>
+
+	<footer id="footer">
+		<?php include("includes/footer.php"); ?>
+	</footer>
+		</div>
+		<?php include("includes/scripts.php"); ?>
+		
+	</body>
+</html>
---
+++ web/includes/footer.php
@@ -0,0 +1,92 @@
+<div class="container">
+	<div class="container">
+  <h2>Our  Partners</h2>
+   <section class="customer-logos slider">
+      <div class="slide"><img src="https://image.freepik.com/free-vector/luxury-letter-e-logo-design_1017-8903.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/3d-box-logo_1103-876.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/blue-tech-logo_1103-822.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/colors-curl-logo-template_23-2147536125.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/abstract-cross-logo_23-2147536124.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/football-logo-background_1195-244.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/background-of-spots-halftone_1035-3847.jpg"></div>
+      <div class="slide"><img src="https://image.freepik.com/free-vector/retro-label-on-rustic-background_82147503374.jpg"></div>
+   </section>
+</div>
+		<div class="row">
+
+			<div class="col-md-3">
+				<div class="newsletter">
+					<h4>¡ Suscríbete  !</h4>
+					<p>Y recibe las noticias más importantes sobre el mundo de las criptomonedas </p>
+
+					<div class="alert alert-success hidden" id="newsletterSuccess">
+						<strong>Success!</strong> You've been added to our email list.
+					</div>
+
+					<div class="alert alert-danger hidden" id="newsletterError"></div>
+
+
+
+
+					<form id="newsletterForm" action method="POST" onsubmit="JsNewsletter();return false;">
+						<input name="key" id="key" type="hidden" value="newsletter">
+						<div class="input-group">
+							<input  type="email" class="form-control" placeholder="Email Address" name="newsletterEmail" id="newsletterEmail" required >
+							<span class="input-group-btn">
+								<button class="btn btn-default" type="submit">Go!</button>
+							</span>
+						</div>
+					</form>
+				</div>
+			</div>
+
+			<div class="col-md-4">
+				<div class="contact-details">
+					<h4>Contacto</h4>
+					<ul class="contact">
+						<li>
+						  <p><em class="fa fa-map-marker"></em> <strong>Address: Ec170401</strong></p>
+						</li>
+						<li><p><i class="fa fa-phone"></i> <strong>Phone:</strong><a href="https://chat.whatsapp.com/AOph3PtoWTT60sVciClpun"> WhatsApp Group</a> </p></li>
+						<li><p><i class="fa fa-envelope"></i> <strong>Email:</strong> <a href="mailto:hola@Sucrecoin.org">hola@sucrecoin.org</a></p></li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-md-2">
+				<h4>Síguenos </h4>
+				<!-- <ul class="social-icons">
+					<li class="social-icons-facebook"><a href="https://www.facebook.com/sucrecoin/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+					<li class="social-icons-twitter"><a href="https://twitter.com/SucreCoin_XSR" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+
+				</ul> -->
+				<ul class="header-social-icons social-icons hidden-xs">
+					<li class="social-icons-facebook"><a href="https://www.facebook.com/sucrecoin/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+					<li class="social-icons-twitter"><a href="https://twitter.com/SucreCoin_XSR" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+					<li class="social-icons-github"><a href="https://github.com/charliegama/sucrecoin" target="_blank" title="Twitter"><i class="fa fa-github"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="footer-copyright">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1">
+					<a href="Sucrecoin" class="logo">
+						<img alt="Porto Website Template" class="img-responsive" src="img/logo-footer.png">
+					</a>
+				</div>
+				<div class="col-md-7">
+					<p>© Copyright 2017 - Desarrollo por VPS593.com para AltCoinsEcuador</p>
+				</div>
+				<div class="col-md-4">
+					<nav id="sub-menu">
+						<ul>
+							<li><a href="WhitePaper-15Noviembre2017-SucreCoin.pdf">Aviso de Privacidad</a></li>
+							<li><a href="WhitePaper-15Noviembre2017-SucreCoin.pdf">Terminos legales</a></li>
+
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+	</div>
